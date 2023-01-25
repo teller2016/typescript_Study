c55c4f4b543f66350312b96cc1c366392da4c373
teller2016 <teller2016@ajou.ac.kr>
2023-01-25 22:55:29 +0900

    javascript to typescript example

---
+++ chapter1/why_typescript/sample.js
@@ -0,0 +1,6 @@
+function add(a, b) {
+    return a + b;
+}
+
+add(10 + '20'); // 1020
+add()
---
+++ chapter1/why_typescript/sample.ts
@@ -0,0 +1,5 @@
+function sum(a: number, b: number): number {
+    return a + b;
+}
+let result = sum(10, 20);
+result.toLocaleString();
