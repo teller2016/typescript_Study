584ccfb428c41a7f1b7d5f7f18333edeff8eb395
teller2016 <teller75@naver.com>
2023-02-01 14:12:39 +0900

    quiz1 solved

--- chapter1/quiz/1_todo/src/index.ts
+++ chapter1/quiz/1_todo/src/index.ts
@@ -1,7 +1,13 @@
-let todoItems;
+interface TodoItem {
+  id: number;
+  title: string;
+  done: boolean;
+}
+
+let todoItems: Array<TodoItem>;
 
 // api
-function fetchTodoItems() {
+function fetchTodoItems(): Array<TodoItem> {
   const todos = [
     { id: 1, title: '안녕', done: false },
     { id: 2, title: '타입', done: false },
@@ -11,43 +17,47 @@ function fetchTodoItems() {
 }
 
 // crud methods
-function fetchTodos() {
+function fetchTodos(): Array<TodoItem> {
   const todos = fetchTodoItems();
   return todos;
 }
 
-function addTodo(todo) {
+function addTodo(todo: TodoItem): void {
   todoItems.push(todo);
 }
 
-function deleteTodo(index) {
+function deleteTodo(index: number): void {
   todoItems.splice(index, 1);
 }
 
-function completeTodo(index, todo) {
+function completeTodo(index: number, todo: TodoItem): void {
   todo.done = true;
   todoItems.splice(index, 1, todo);
 }
 
 // business logic
-function logFirstTodo() {
+function logFirstTodo(): TodoItem {
   return todoItems[0];
 }
 
-function showCompleted() {
+function showCompleted(): Array<TodoItem> {
   return todoItems.filter(item => item.done);
 }
 
-// TODO: 아래 함수의 내용을 채워보세요. 아래 함수는 `addTodo()` 함수를 이용하여 2개의 새 할 일을 추가하는 함수입니다.
-function addTwoTodoItems() {
-  // addTodo() 함수를 두 번 호출하여 todoItems에 새 할 일이 2개 추가되어야 합니다.
+function addTwoTodoItems(todos: Array<TodoItem>): void {
+  todos.forEach(todo => {
+    addTodo(todo);
+  });
 }
 
 // NOTE: 유틸 함수
-function log() {
+function log(): void {
   console.log(todoItems);
 }
 
 todoItems = fetchTodoItems();
-addTwoTodoItems();
+addTwoTodoItems([
+  { id: 1, title: '안녕', done: false },
+  { id: 2, title: '타입', done: false },
+]);
 log();
