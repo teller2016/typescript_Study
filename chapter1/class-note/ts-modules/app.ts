eaa984f6407e8e2a44e358d03d0556d14355bc0f
teller2016 <teller2016@ajou.ac.kr>
2023-02-10 21:58:05 +0900

    type module study

---
+++ chapter1/class-note/ts-modules/app.ts
@@ -0,0 +1,6 @@
+import { Todo } from './types';
+
+var item: Todo = {
+    title: '할일 1',
+    checked: false,
+}
---
+++ chapter1/quiz/2_address-book/src/types.ts
@@ -0,0 +1,23 @@
+interface PhoneNumberDictionary {
+    [phone: string]: {
+      num: number;
+    };
+  }
+  
+interface Contact {
+    name: string;
+    address: string;
+    phones: PhoneNumberDictionary;
+  }
+  
+enum PhoneType {
+    Home = 'home',
+    Office = 'office',
+    Studio = 'studio',
+  }
+
+export {
+    PhoneNumberDictionary,
+    Contact,
+    PhoneType,
+}
